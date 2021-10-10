650da801a8e2086818115d37e26decfb3b9f4540
Ezra Samuel <samuelezra234@gmail.com>
2021-10-10 22:28:08 +0100

    removing env var

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-const udacityProgram = process.env.UDACITY_PROGRAM;
-console.log('The program I am doing with Udacity is ${udacityProgram}');
 
 async function main() {
   while(true) {
